9c536cb1bfed5edee6137bd48946fffc339fe29c
Benjamin Arce <Benjamin_Arce@epam.com>
2020-10-08 09:48:47 -0500

    [add] - adding mochawesome

--- protractor.config.js
+++ protractor.config.js
@@ -13,23 +13,12 @@ exports.config = {
 
   specs: ["./specs/api.spec.js"],
 
-  /*
-  jasmineNodeOpts: {
-    showColors: true, // Use colors in the command line report.
-    defaultTimeoutInterval: 30000, // Default time to wait in ms before a test fails.
-  },*/
-  mochaOpts: { // Some reasonable mocha config
+  mochaOpts: {
     reporter: "spec",
     slow: 3000,
     ui: 'bdd',
     timeout: 30000
   },
 
-  onPrepare: function () {
-    // making chai available globally. in your test use `const expect = global['chai'].expect;`
-    //var chai = require("chai");
-    //var chaiAsPromised = require("chai-as-promised");
-    //chai.use(chaiAsPromised);
-    //global.chai = chai;
-  },
+  onPrepare: function () {},
 };
