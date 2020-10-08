16fa161c60f0999f63857fc9494a3b73a6dad6e8
Benjamin Arce <Benjamin_Arce@epam.com>
2020-10-08 12:21:18 -0500

    [add] - new scenario for checking keys

--- specs/api.spec.js
+++ specs/api.spec.js
@@ -11,18 +11,21 @@ describe("/GET - JSON API", function () {
   });
 
   it("Test to verify a http status code", async function () {
-    console.log(1)
     expect(res.status).to.equal(200)
   });
 
   it("Test to verify a http response header", async function () {
-    console.log(2)
     expect(res.headers).to.have.property("content-type");
     expect(res.headers["content-type"]).to.equal("application/json; charset=utf-8");
   });
 
   it("test to verify a http response body", async function () {
-    console.log(3)
     expect(res.body).to.have.lengthOf(10);
+  });
+
+  it.only("test to validates http response body keys", async function () {
+    res.body.forEach(element => {
+      expect(element).to.have.all.keys('id', 'name', 'address', 'company', 'email', 'phone', 'username', 'website');
+    });
   });
 });
